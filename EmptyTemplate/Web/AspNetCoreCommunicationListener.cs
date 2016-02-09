ad3833d03106921e3bea5aed4f08c59777586d6c
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-02-08 16:21:20 -0800

    Initial commit

---
+++ EmptyTemplate/Web/AspNetCoreCommunicationListener.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Features;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class AspNetCoreCommunicationListener : ICommunicationListener
+    {
+        private readonly IWebHost _webHost;
+
+        public AspNetCoreCommunicationListener(IWebHost webHost)
+        {
+            if (webHost == null)
+            {
+                throw new ArgumentNullException(nameof(webHost));
+            }
+
+            _webHost = webHost;
+        }
+
+        public void Abort()
+        {
+            _webHost.Dispose();
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            _webHost.Dispose();
+
+            return Task.FromResult(true);
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            _webHost.Start();
+
+            var serverAddressesFeature = _webHost.ServerFeatures.Get<IServerAddressesFeature>();
+
+            return Task.FromResult(string.Join(";", serverAddressesFeature.Addresses.Select(p => AddressUtilities.GetPublishingAddress(p))));
+        }
+    }
+}
